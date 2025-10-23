f1157a3009f7d9b2fe5a5727ae905dd832b9ddd3
Fayruz10 <zamzamymuhammadfayruz@gmail.com>
2025-10-23 15:48:00 +0000

    Add files via upload

---
+++ config.rs
@@ -0,0 +1,11 @@
+//! File konfigurasi MQTT & WiFi
+
+pub const WIFI_SSID: &str = "hotspotesp";
+pub const WIFI_PASS: &str = "12345666";
+
+/// Endpoint ThingsBoard (bisa on-prem atau cloud)
+pub const THINGSBOARD_HOST: &str = "mqtt.thingsboard.cloud";
+pub const THINGSBOARD_PORT: u16 = 1883;
+
+/// Token device yang didapat dari ThingsBoard
+pub const ACCESS_TOKEN: &str = "aPuWIWWHB8fOjsU9Uugp";
